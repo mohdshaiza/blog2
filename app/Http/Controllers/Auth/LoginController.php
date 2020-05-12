3480a8d8daff5329d234fb0a93add8d0b202f6ba
Mohd Shaiza <mohdshaiza@gmail.com>
2020-05-12 10:28:41 +0800

    Overwrite Login method.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,28 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function login(Request $request)
+    {
+        $input = $request->all();
+
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $fieldType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        if (auth()->attempt(array($fieldType => $input['username'], 'password' => $input['password']))) {
+            return redirect()->route('home');
+        } else {
+            return redirect()->route('login')->with('error', 'Email-Address and Password Are Wrong.');
+        }
+
+    }
 }
